fd59f1c30d61eee5c18e2d39c1ee48d390582cfd
Akash Dhopte <akashdhopte80@gmail.com>
2020-08-15 17:02:41 +0530

    final changes

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger'
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('.header');
-// .header .nav-bar .nav-list ul li a'
 hamburger.addEventListener('click',()=>{
     hamburger.classList.toggle('active');
     mobile_menu.classList.toggle('active');
